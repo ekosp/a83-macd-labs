b98584668c94342954ebb805b0f1f126fd3f351c
Eko Setyo Purnomo <ekosetyopurnomo@gmail.com>
2019-05-09 13:18:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "ekospdbserver.database.windows.net";
+    $user = "ekosp";
+    $pass = "Freedom4392~";
+    $db = "ekospdb";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
